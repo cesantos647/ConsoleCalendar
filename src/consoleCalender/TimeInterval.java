dfbfbfda5798d0ab5fd320422c92cde28ac41b0c
Christian Santos <cesantos647@gmail.com>
2020-06-20 02:52:41 -0700

    Implement TimeInterval class

--- src/consoleCalender/TimeInterval.java
+++ src/consoleCalender/TimeInterval.java
@@ -1,8 +1,33 @@
 package consoleCalender;
 
+import java.time.LocalTime;;
+
 public class TimeInterval {
-	//Change start and end time to appropriate data type
-	private String startTime;
-	private String endTime;
+	private LocalTime startTime;
+	private LocalTime endTime;
+	
+	public TimeInterval(LocalTime st, LocalTime et) {
+		startTime = st;
+		endTime = et;
+	}
+
+	/**
+	 * @return the startTime
+	 */
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+
+	/**
+	 * @return the endTime
+	 */
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+	
+	//Not sure if this is right, please check again.
+	public boolean conflictsWith(TimeInterval that) {
+		return startTime.compareTo(that.getEndTime()) > 0 && endTime.compareTo(that.getStartTime()) < 0;		
+	}
 
 }

207ae6fcdf5e0d489be34e1693ab793c7ae6eacf
Christian Santos <cesantos647@gmail.com>
2020-06-20 02:53:08 -0700

    create skeleton for the MyCalendar class

--- src/consoleCalender/MyCalendar.java
+++ src/consoleCalender/MyCalendar.java
@@ -1,9 +1,72 @@
 package consoleCalender;
 import java.util.HashMap;
 import java.util.TreeSet;
+import java.time.LocalTime;
+import java.time.LocalDate;
 
 public class MyCalendar {
 	private HashMap<String, TreeSet<Event>> eventsList;
 	//Read + create initial events
 	//View, Create, Go to, Event List, Delete Event, Quit
+	public void displayMainMenu() {
+		
+	}
+	public void view() {
+		
+	}
+	
+	public void goTo() {
+		
+	}
+	
+	public void eventList() {
+		
+	}
+	
+	public void createEvent() {
+		
+	}
+	
+	public void deleteEvent() {
+		
+	}
+	
+	public void quit() {
+		
+	}
+	public void dayView(String date) {
+		
+	}
+	public void monthView(String month) {
+		
+	}
+	public boolean addEvent() {
+		return false;
+		
+	}
+	public void deleteSingleEvent() {
+		
+	}
+	public void deleteAllOnDate() {
+		
+	}
+	public void deleteRecurring() {
+		
+	}
+	public void saveEvents() {
+		
+	}
+	public LocalDate askDate() {
+		return null;
+		
+	}
+	public String askName() {
+		return null;
+	}
+	public LocalTime askStartTime() {
+		return null;
+	}
+	public LocalTime askEndTime(LocalDate startTime) {
+		return null;
+	}
 }
